a761366ecc652bb90ca75383ef873df8b9e1cfeb
SteaceP <steacy@live.ca>
2026-01-18 16:34:12 -0500

    refactor: introduce RepairPart interface and update Repair type to use it for parts.

--- src/types/appliances.ts
+++ src/types/appliances.ts
@@ -4,4 +4,15 @@ export type ApplianceStatus = Database["public"]["Enums"]["appliance_status"];
 
 export type Appliance = Database["public"]["Tables"]["appliances"]["Row"];
 
-export type Repair = Database["public"]["Tables"]["repairs"]["Row"];
+export interface RepairPart {
+  id?: string;
+  name: string;
+  price: number;
+}
+
+export type Repair = Omit<
+  Database["public"]["Tables"]["repairs"]["Row"],
+  "parts"
+> & {
+  parts: RepairPart[] | null;
+};
