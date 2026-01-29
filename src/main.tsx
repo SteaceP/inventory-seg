f5b8bc16e5f2f1ecebf2f3fc44939405d54f8ae9
SteaceP <steacy@live.ca>
2026-01-29 05:42:18 -0500

    perf: Decrease Sentry traces and session replay sampling rates.

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ Sentry.init({
     Sentry.replayIntegration(),
   ],
   // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  tracesSampleRate: 0.1, //  Capture 10% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
   tracePropagationTargets: [
     "localhost",
@@ -22,7 +22,7 @@ Sentry.init({
     /^https:\/\/nidebcwouohczncxrgfv\.supabase\.co/,
   ],
   // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysSessionSampleRate: 0.01, // This sets the sample rate at 1%.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   sendDefaultPii: true,
 });
