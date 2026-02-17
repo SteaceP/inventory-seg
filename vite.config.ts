c3f222bd72d29b53f3beae9acc3eafec57708ec0
SteaceP <steacy@live.ca>
2026-02-16 23:15:51 -0500

    refactor: add type annotations for vite plugin options and transformIndexHtml parameter

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
 import UnpluginFonts from "unplugin-fonts/vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv, type PluginOption } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import mkcert from "vite-plugin-mkcert";
 import { VitePWA } from "vite-plugin-pwa";
@@ -112,7 +112,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       {
         name: "html-env-transform",
-        transformIndexHtml(html) {
+        transformIndexHtml(html: string) {
           return html
             .replace(
               /%VITE_APP_NAME%/g,
@@ -282,7 +282,7 @@ export default defineConfig(({ mode }) => {
             }),
           ]
         : []),
-    ],
+    ] as PluginOption[],
     optimizeDeps: {
       include: ["@emotion/react", "@emotion/styled", "@mui/material/Tooltip"],
     },
